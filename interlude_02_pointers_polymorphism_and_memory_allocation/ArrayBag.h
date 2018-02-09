2b2e08aca1f515507ba77bb79df5f01ba6f86296
zwpdbh <hyperion_z@outlook.com>
2018-02-09 21:08:39 +1300

    < finish interlude 2 > chapter 4

---
+++ interlude_02_pointers_polymorphism_and_memory_allocation/ArrayBag.h
@@ -0,0 +1,33 @@
+//
+// Created by zwpdbh on 20/11/2017.
+//
+
+#ifndef PROBLEM_SOLVING_ARRAYBAG_H
+#define PROBLEM_SOLVING_ARRAYBAG_H
+
+#include "../interfaces/BagInterface.h"
+
+template <class T>
+class ArrayBag : BagInterface<T> {
+private:
+    static const int DEFAULT_CAPACITY = 6;
+    T items[DEFAULT_CAPACITY];
+    int itemCount;
+    int maxItems;
+
+    int getIndexOf(const T& target) const;
+
+public:
+    ArrayBag();
+    int getCurrentSize() const;
+    bool isEmpty() const;
+    bool add(const T& newEntry);
+    bool remove(const T& anEntry);
+    void clear();
+    bool contains(const T& anEntry) const;
+    int getFrequencyOf(const T& anEntry) const;
+    std::vector<T> toVector() const;
+};
+
+#include "ArrayBag.cpp"
+#endif //PROBLEM_SOLVING_ARRAYBAG_H
---
+++ interlude_02_pointers_polymorphism_and_memory_allocation/dome_shows_resizable_array.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zwpdbh on 09/02/2018.
+//
+
+
