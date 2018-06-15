8d23c4aaa4985de7f859cbbc7a7abaca30f2c137
zwpdbh <hyperion_z@outlook.com>
2018-06-15 15:40:47 +1200

    < add a comparison between different print function

---
+++ C_C++_fundamental/build/CMakeFiles/different_print_function.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/different_print_function.dir/different_print_function.cpp.o"
+  "../bin/different_print_function.pdb"
+  "../bin/different_print_function"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/different_print_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ bin/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_4cba2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_4cba2.dir/feature_tests.cxx.o"
+  "cmTC_4cba2.pdb"
+  "cmTC_4cba2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_4cba2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
