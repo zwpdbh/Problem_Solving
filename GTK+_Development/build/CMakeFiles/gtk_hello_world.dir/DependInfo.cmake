fa2d1ba0d5f546ac6c120985d6318cb6ffc7ac39
zwpdbh <hyperion_z@outlook.com>
2018-09-14 11:22:12 +0800

    >gtk+3.0 for FAF

---
+++ GTK+_Development/build/CMakeFiles/gtk_hello_world.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zw/code/C_and_C++_Projects/Problem_Solving/GTK+_Development/hello_world.cpp" "/Users/zw/code/C_and_C++_Projects/Problem_Solving/GTK+_Development/build/CMakeFiles/gtk_hello_world.dir/hello_world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_REENTRANT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/usr/local/Cellar/gtk+3/3.22.30/include/gtk-3.0"
+  "/usr/local/Cellar/glib/2.58.0_1/include/gio-unix-2.0"
+  "/usr/local/Cellar/cairo/1.14.12/include/cairo"
+  "/usr/local/Cellar/libepoxy/1.5.2/include"
+  "/usr/local/Cellar/pango/1.42.4/include/pango-1.0"
+  "/usr/local/Cellar/harfbuzz/1.9.0/include/harfbuzz"
+  "/usr/local/Cellar/graphite2/1.3.12/include"
+  "/usr/local/Cellar/fribidi/1.0.5/include/fribidi"
+  "/usr/local/Cellar/atk/2.30.0/include/atk-1.0"
+  "/usr/local/Cellar/pixman/0.34.0_1/include/pixman-1"
+  "/usr/local/Cellar/fontconfig/2.13.1/include"
+  "/usr/local/opt/freetype/include/freetype2"
+  "/usr/local/Cellar/libpng/1.6.35/include/libpng16"
+  "/usr/local/Cellar/gdk-pixbuf/2.38.0/include/gdk-pixbuf-2.0"
+  "/usr/local/Cellar/glib/2.58.0_1/include/glib-2.0"
+  "/usr/local/Cellar/glib/2.58.0_1/lib/glib-2.0/include"
+  "/usr/local/opt/gettext/include"
+  "/usr/local/Cellar/pcre/8.42/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ GTK+_Development/build/CMakeFiles/gtk_hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtk_hello_world.dir/hello_world.cpp.o"
+  "../bin/gtk_hello_world.pdb"
+  "../bin/gtk_hello_world"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtk_hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
